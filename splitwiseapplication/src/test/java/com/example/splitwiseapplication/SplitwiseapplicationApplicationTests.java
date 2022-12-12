d12fe31f7ee05dcf733c8676e0efd7f39e016b9b
vishalgggg <vsoftjob184@gmail.com>
2022-12-12 04:08:57 +0000

    created spring

---
+++ splitwiseapplication/src/test/java/com/example/splitwiseapplication/SplitwiseapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.splitwiseapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitwiseapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
